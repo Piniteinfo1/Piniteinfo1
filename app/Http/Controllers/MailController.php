9530928c0f5cc8201c9fd2f6254ffc577a4fe0f5
piniteinfo1 <info@piniteinfo.com>
2022-03-27 04:48:02 -0700

    login page and registration page

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class MailController extends Controller
+{
+    public function SendMail()
+    {	
+      $data = array('name'=>"Virat Gandhi");
+    	//dd('email check');
+   
+      $mail = Mail::send(['html'=>'mail'], $data, function($message) {
+         $message->to('chanduakula111@gmail.com', 'Tutorials Point')->subject
+            ('Laravel Basic Testing Mail');
+         $message->from('xyz@gmail.com','Virat Gandhi');
+      });
+      echo "Basic Email Sent. Check your inbox.";
+    }
+}
